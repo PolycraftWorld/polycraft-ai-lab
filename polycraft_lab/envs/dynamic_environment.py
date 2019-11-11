8a79c20014f9b8d8553ef7eadca4103caae4004d
Willie Chalmers III <willie.chalmers@polycraftworld.com>
2019-11-11 17:46:58 -0600

    Update DynamicExperimentEnv to use new PolycraftClient
    TODO: Setup more stubs for commonly-used server commands
    TODO: Adversarial-based novelty generation

--- polycraft_lab/envs/dynamic_environment.py
+++ polycraft_lab/envs/dynamic_environment.py
@@ -1,6 +1,13 @@
+import logging
 from typing import Tuple
 
 from gym import Env
+
+from installation.manager import PolycraftInstallation
+from polycraft_lab.envs.experiment_config import ExperimentConfig
+from polycraft_lab.installation.client import PolycraftClient
+
+log = logging.getLogger(__name__)
 
 
 class DynamicExperimentEnv(Env):
@@ -24,7 +31,26 @@ class DynamicExperimentEnv(Env):
     metadata = {'render.modes': ['human, state_pixels, rgb_array', 'ansi']}
 
     def __init__(self, config_path: str):
+        self.config = self._load_experiment_config(config_path)
+        log.info('Config fetched.')
+        installation = PolycraftInstallation()
+        try:
+            if not installation.is_installed:  # Just in case pip install didn't work
+                installation.install()  # TODO: Probably just wrap this in a "ensure_installed" function
+            self.client = PolycraftClient(installation.client_location)
+            self.action_space = self.config.action_space
+        except Exception:
+            raise Exception('Cannot start training. Error during Polycraft runtime installation.')
+        self.client.start()
         self.reward = 0
+
+    @staticmethod
+    def _load_experiment_config(config_path: str):
+        try:
+            return ExperimentConfig.from_file(config_path)
+        except IOError as e:
+            log.error('Could not get config from file.', e)
+            raise e
 
     def step(self, action) -> Tuple[object, float, bool, dict]:
         """Compute a training step.
@@ -37,35 +63,44 @@ class DynamicExperimentEnv(Env):
             and info.
         """
         observation = {}
-        reward = self._determine_reward(action)
-        done = self._is_done
+        reward = self._determine_reward(action, observation)
+        done = False
         info = {}  # TODO: Find useful information to put in her
         return observation, reward, done, info
 
     def reset(self):
         """Prepare the environment for another episode of training."""
-        raise NotImplementedError()
+        # TODO: Provide functionality to put experiments in sequence
+        # One episode might consist of multiple scenes in a sequence?
+        scenes = self.config.scenes  # Probably domains for now
+        self.client.setup_scene(scenes[0])
 
-    def render(self, mode='human'):
-        """Display """
+    def render(self, mode: str = 'human'):
+        """Display training output for this environment.
+
+        Args:
+            mode (str): How this environment should output information
+        """
         assert mode in self.metadata['render.modes']
         if mode == 'human':
             pass
         elif mode == 'state_pixels':
             pass
-        else:  # mode == rgb_array
+        elif mode == 'rgb_array':
+            pass
+        else:  # mode == 'ansi'
             pass
         raise NotImplementedError()
 
     def close(self):
+        """Shutdown this training environment."""
+        self.client.stop()
+
+    def _determine_reward(self, state, action):
         raise NotImplementedError()
 
-    @property
-    def _is_done(self) -> bool:
-        return False
-
-    def _setup_scene(self, scene: str):
-        raise NotImplementedError()
-
-    def _determine_reward(self, action):
-        raise NotImplementedError()
+    def _determine_state(self):
+        action_space = self.action_space
+        # TODO: Use novelty.
+        self.client.send_to_server('GENERATE NOVELTY')  # Okay, not really.
+        pass

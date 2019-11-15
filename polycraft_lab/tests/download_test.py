5a162a9e26a5af49e07e30885acf7ebc06059139
Willie Chalmers III <willie.chalmers@polycraftworld.com>
2019-11-15 17:34:48 -0600

    Update tests for mod downloading and installation

--- polycraft_lab/tests/download_test.py
+++ polycraft_lab/tests/download_test.py
@@ -1,22 +1,37 @@
+import socket
 import unittest
 
-from polycraft_lab.installation.download import download_polycraft
+from polycraft_lab.installation.download import download_and_extract_polycraft
 from polycraft_lab.tests import POLYCRAFT_TESTS_DIR
 
 
 class DownloadModTestCase(unittest.TestCase):
     """Verify all download functions work correctly."""
 
-    DOWNLOAD_DIRECTORY = POLYCRAFT_TESTS_DIR / 'polycraftworld-mod'
+    DOWNLOAD_DIRECTORY = POLYCRAFT_TESTS_DIR / 'polycraft-download'
 
-    INSTALLATION_DIRECTORY = POLYCRAFT_TESTS_DIR / 'polycraftworld-mod'
+    INSTALLATION_DIRECTORY = POLYCRAFT_TESTS_DIR / 'polycraft'
+
+    TEST_REPO_LOCATION = 'https://filebin.net/p5rcppoqfxiipiak/polycraft-1.8.9AIGym.zip?t=01f0elf7'
 
     def test_mod_download(self):
-        download_polycraft(self.DOWNLOAD_DIRECTORY)
+        """
+        Should download Polycraft zip to DOWNLOAD_DIRECTORY and extract files to
+        INSTALLATION_DIRECTORY
+        """
+        download_and_extract_polycraft(self.INSTALLATION_DIRECTORY,
+                                       download_directory=self.DOWNLOAD_DIRECTORY,
+                                       repo_location=self.TEST_REPO_LOCATION)
         # Check if downloads directory contains files
 
     def fail_download(self):
         """TODO: Test a network failure when downloading"""
+        guard = lambda: exec('raise Exception(\'No network connection\')')
+        socket.socket = guard
+        with self.assertRaises(Exception) as context:
+            download_and_extract_polycraft(self.INSTALLATION_DIRECTORY,
+                                           download_directory=self.DOWNLOAD_DIRECTORY)
+        self.assertTrue(context.exception)
 
 
 if __name__ == '__main__':

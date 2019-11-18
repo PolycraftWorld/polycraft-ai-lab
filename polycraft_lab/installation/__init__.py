2bc2b4c445cc9969352e43ef7a23e08d34ffcbe0
Willie Chalmers III <willie.chalmers@polycraftworld.com>
2019-11-18 16:28:19 -0600

    Add useful constants to installation.__init__.py
    This centralizes all important paths to the installation module's __init__.py
    and creates a logger with namespace 'pal.installer'

--- polycraft_lab/installation/__init__.py
+++ polycraft_lab/installation/__init__.py
@@ -1,3 +1,17 @@
 """Modules used to manage and run the Polycraft World installation."""
+import logging
+import tempfile
+from pathlib import Path
 
-__all__ = ['client', 'client', 'config', 'manager', 'post_pip_install']
+__all__ = [
+    'client', 'client', 'config', 'manager', 'post_pip_install',
+    'PAL_MOD_DIR_NAME', 'PAL_LAB_DIR_NAME', 'PAL_TEMP_PATH'
+]
+
+log = logging.getLogger('pal').getChild('installer')
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())
+
+PAL_TEMP_PATH = Path(tempfile.gettempdir()) / 'polycraft'
+PAL_MOD_DIR_NAME = 'polycraft-world'
+PAL_LAB_DIR_NAME = 'polycraft-lab'

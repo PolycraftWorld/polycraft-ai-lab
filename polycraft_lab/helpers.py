6752e2dcb77e66516891335b911c00c47a6c3f6e
Willie Chalmers III <willie.chalmers@polycraftworld.com>
2019-11-18 17:32:06 -0600

    Add logging to helpers and update docs

--- polycraft_lab/helpers.py
+++ polycraft_lab/helpers.py
@@ -1,4 +1,5 @@
 """Some helper functions for various Polycraft AI Lab tasks."""
+import logging
 
 import gym
 from gym.envs.registration import register
@@ -10,14 +11,17 @@ register(
     entry_point='polycraft_lab.envs.dynamic_environment:DynamicExperimentEnv',
 )
 
+log = logging.getLogger('pal')
+
 
 def setup_env(config_path: str, env_name: str = DYNAMIC_ENV_ID) -> gym.core.Env:
     """Create a dynamic Polycraft AI Lab environment.
 
-    This is used to register an environment in OpenAI Gym before
+    This is used to automatically register an environment in OpenAI Gym before
+    instantiating an instance of it.
 
     Args:
-        config_path (str): The path to the
+        config_path (str): The path to the experiment's configuration file
         env_name (str): By default, 'PALDynamicEnv-0', the standard Polycraft
                         dynamic experiment environment. This is used to load
                         custom experiment environments.
@@ -28,4 +32,5 @@ def setup_env(config_path: str, env_name: str = DYNAMIC_ENV_ID) -> gym.core.Env:
             kwargs={'config_path': config_path}
         )
     env = gym.make(env_name, config_path=config_path)  # Only create env after registration
+    log.info('Registered new environment %s using config at %s', env_name, config_path)
     return env
